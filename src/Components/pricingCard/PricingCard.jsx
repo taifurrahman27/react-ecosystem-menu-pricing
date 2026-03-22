db1a51d85222b8058e9f9729e202f18e569b04af
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-03-23 02:56:51 +0600

    Subscribe button added

--- src/Components/pricingCard/PricingCard.jsx
+++ src/Components/pricingCard/PricingCard.jsx
@@ -7,14 +7,14 @@ const PricingCard = ({ pricing }) => {
     const { name, price, description, features } = pricing;
 
     return (
-        <div className='border bg-fuchsia-700 rounded-2xl p-5'>
+        <div className='flex flex-col border border-indigo-700 bg-violet-800 rounded-2xl p-5'>
 
             <div >
                 <h1 className='text-6xl'>{name}</h1>
                 <h4 className='text-3xl'>{price}</h4>
 
             </div>
-            <div className='bg-amber-500 p-5 rounded-2xl mt-8'>
+            <div className='bg-amber-700 p-5 rounded-2xl mt-8 flex-1'>
                 <p>{description}</p>
                 {
                     features.map((features, index) => <PricingFeature key={index} features={features} >
@@ -22,6 +22,8 @@ const PricingCard = ({ pricing }) => {
                 }
 
             </div>
+
+            <button className="btn btn-soft btn-success w-full mt-2.5 rounded-2xl">Subscribe</button>
         </div>
     );
 };
